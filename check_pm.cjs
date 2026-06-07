9bba3f2f3253ec998bbf646cc1ce9c73b6440406
Khalil Chergui <khalilchergui2005@gmail.com>
2026-06-07 23:32:40 +0200

    chore: remove temp dev scripts

--- check_pm.cjs
+++
@@ -1,5 +0,0 @@
-﻿const {PrismaClient}=require('@prisma/client');
-const db=new PrismaClient();
-db.siteConfig.findUnique({where:{key:'payment_methods'}})
-  .then(r=>console.log(JSON.stringify(r)))
-  .finally(()=>db.$disconnect());
